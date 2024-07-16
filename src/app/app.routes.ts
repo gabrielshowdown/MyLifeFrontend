bfc242f20f0653ea64439f9dc8965ac87e7b55fc
Gabriel Henrique de Bona <gabriell.hhenrique@hotmail.com>
2024-07-16 20:51:38 -0300

    Criando protótipo de tela de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,20 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { TesteComponent } from './componentes/teste/teste.component';
 import { Teste2Component } from './componentes/teste2/teste2.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'teste',
     component: TesteComponent
   },
   {
