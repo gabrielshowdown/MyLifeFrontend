2c800346d419a5f74a4fcb2617aa8b0cd6135784
Gabriel Henrique de Bona <gabriell.hhenrique@hotmail.com>
2024-07-16 20:04:26 -0300

    Validando rotas da aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TesteComponent } from './componentes/teste/teste.component';
+import { Teste2Component } from './componentes/teste2/teste2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TesteComponent
+  },
+  {
+    path: 'teste2',
+    component: Teste2Component
+  },
+];
