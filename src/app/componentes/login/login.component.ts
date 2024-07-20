5a3c278c43a87cbeac96eca23a4e26b4a7cb9f93
Gabriel Henrique de Bona <gabriell.hhenrique@hotmail.com>
2024-07-19 21:19:25 -0300

    Avançando no formulário de login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  //Atribrutos
+  formulario!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ){}
+
+  ngOnInit(): void{
+    this.formulario = this.formBuilder.group({
+      usuario: ['', Validators.compose([
+        Validators.required, // Não permite branco
+        Validators.pattern(/(.|\s)*\S(.|\s)*/), // Não permite espaços em branco no conteúdo
+        Validators.minLength(3),
+      ])],
+      senha: ['', Validators.compose([
+        Validators.required, // Não permite branco
+        Validators.pattern(/(.|\s)*\S(.|\s)*/), // Não permite espaços em branco no conteúdo
+      ])],
+    })
+  }
+
+  logar(): void{
+    console.log('Clicou no login');
+    //console.log(this.formulario);
+    console.log(this.formulario.get('usuario')?.value);
+    console.log(this.formulario.valid);
+
+  }
 }
