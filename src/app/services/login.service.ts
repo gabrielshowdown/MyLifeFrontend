d3e1752633c4d10105f1c3723e6e32718a540d43
Gabriel Henrique de Bona <gabriell.hhenrique@hotmail.com>
2024-12-09 12:23:43 -0300

    Configurando o service para receber dados do BackEnd.

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,12 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
+  private readonly API_USERS = 'http://localhost:8080/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.API_USERS);
+  }
 
   validateLogin(credentials: { username: string; password: string }) : boolean{
     const { username, password } = credentials
